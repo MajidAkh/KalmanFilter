ac2cc97b63c806eb8b4d4139731a2b728add499a
MajidAkh <maibnha@gmail.com>
2020-10-08 21:24:52 +0200

    verification de la cohérence des résultats  ainsi que traçage de la vitesse de la position et de leurs incertitude

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 import numpy as np 
+import matplotlib.pyplot as plt 
+from kf import KF 
 
-class KF:
-    def __init__(self, initial_x, initial_v):
-        # mean of state GRV
-        self.x = np.array([initial_x, initial_v])
+plt.ion()
+plt.figure()
+kf = KF(initial_x = 0.0, initial_v = 1.0, accel_variance= 0.1)
 
-        #covariance of state GRV
-        self.P = np.eye(2)
-kf = KF(initial_x = 0.2, initial_v = 0.5)
+DT = 0.1
+NUM_STEPS = 1000
 
+moys = []
+covs = []
+
+for i in range(NUM_STEPS):
+    covs.append(kf.cov)
+    moys.append(kf.mean)
+
+    kf.predict(dt = DT)
+
+
+plt.subplot(2,1,1)
+plt.title("Position")
+plt.plot([mu[0] for mu in moys], 'r')
+plt.plot([mu[0] - 2*np.sqrt(cov[0,0]) for mu, cov in zip(moys, covs)], 'r--')
+plt.plot([mu[0] + 2*np.sqrt(cov[0,0]) for mu, cov in zip(moys, covs)], 'r--')
+
+
+
+plt.subplot(2,1,2)
+plt.title("Velocity")
+plt.plot([mu[1] for mu in moys], 'r')
+plt.plot([mu[1] - 2*np.sqrt(cov[1,1]) for mu, cov in zip(moys, covs)], 'r--')
+plt.plot([mu[1] + 2*np.sqrt(cov[1,1]) for mu, cov in zip(moys, covs)], 'r--')
+
+
+plt.show()
+plt.ginput(1)
--- test_kf.py
+++ test_kf.py
@@ -1,5 +1,6 @@
 from kf import KF
 import unittest
+import numpy as np
 
 class TestKF(unittest.TestCase):
     def test_can_construct_with_x_and_v(self):
@@ -17,4 +18,29 @@ class TestKF(unittest.TestCase):
 
         kf.predict(dt = 0.1)
 
+    def test_after_calling_predict_mean_and_cov_are_of_right_shape(self):
+        x = 0.2
+        v = 2.3
+
+        kf = KF(initial_x = x, initial_v = v, accel_variance= 1.2)
+
+        kf.predict(dt = 0.1)
+        self.assertEqual(kf.cov.shape, (2,2))
+        self.assertEqual(kf.mean.shape, (2,))
+        
+
+    def test_after_calling_predict_increases_state_uncertainty(self):
+        x = 0.2
+        v = 2.3
+        temp = 0
+        kf = KF(initial_x = x, initial_v = v, accel_variance= 1.2)
+        for  i in range(10):
+            #l'entropie de la matrice de covariance est exprimé par son determinant, ici je m'attend à obtenir un determinant qui croit.
+            #l'incertitude devant être de plus en plus grande au cours du temps.
+            det_before = np.linalg.det(kf.cov)
+            kf.predict(dt=0.1)
+            det_after = np.linalg.det(kf.cov)
+            
+            self.assertGreater(det_after, det_before)
+            print(det_before, det_after)
 
