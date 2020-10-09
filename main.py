1e9e12f54504722835b598d515a70d6eb4b89ec9
MajidAkh <maibnha@gmail.com>
2020-10-09 14:03:47 +0200

    fonction de mise à jour du modèle de prédiction + fonction test + image de la qualité de la prévision

--- main.py
+++ main.py
@@ -8,15 +8,29 @@ kf = KF(initial_x = 0.0, initial_v = 1.0, accel_variance= 0.1)
 
 DT = 0.1
 NUM_STEPS = 1000
+MEAS_EVERY_STEPS = 20
+
+real_x = 0.0
+meas_variance = 0.1**2
+real_v = 0.9
 
 moys = []
 covs = []
 
-for i in range(NUM_STEPS):
+for step in range(NUM_STEPS):
     covs.append(kf.cov)
     moys.append(kf.mean)
 
+    real_x = real_x + DT*real_v
+
     kf.predict(dt = DT)
+    if step != 0 and step % MEAS_EVERY_STEPS == 0:
+        kf.update(meas_value = real_x + np.random.randn() * np.sqrt(meas_variance), 
+                    meas_variance = meas_variance)
+
+
+
+
 
 
 plt.subplot(2,1,1)
